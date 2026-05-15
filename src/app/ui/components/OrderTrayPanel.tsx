faf614778c9868b03314d68f7cf75d02f1fb6c2e
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2026-05-15 21:01:17 +0000

    OrderTrayPanel We changed the Order Tray Panel and made changes to Favicon

--- src/app/ui/components/OrderTrayPanel.tsx
+++ src/app/ui/components/OrderTrayPanel.tsx
@@ -77,11 +77,11 @@ const OrderTrayPanel = () => {
                 >
                     {/*<TrayIcon size={24} className="text-white" />*/}
                     {/* Badge showing number of items when there are items in the order */}
-                    {getTotalItems() > 0 && (
+                    {/*{getTotalItems() > 0 && (
                         <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
                             {getTotalItems()}
                         </span>
-                    )}
+                    )}*/}
                 </Button>
             </div>
 
